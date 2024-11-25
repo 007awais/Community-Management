d36edd824e1182e89e85b7c93c391246f64d87f9
007awais <116508346+007awais@users.noreply.github.com>
2024-11-25 14:24:28 +0500

    First Commit

---
+++ FX_TRY/src/application/Admin.java
@@ -0,0 +1,34 @@
+package application;
+
+
+public class Admin{
+	
+	
+	    private int adminID;
+	    private String username;
+	    private String password;
+	    private String permissions;
+	    
+	    
+	    // Getter and Setter for permissions
+	    public String getPermissions() {
+	        return permissions;
+	    }
+
+	    public void setPermissions(String permissions) {
+	        this.permissions = permissions;
+	    }
+	    
+	  
+	 public void updateRecords()
+	 {
+		 //updates the record of the customers
+	 }
+	 
+	 
+	 
+	  
+}
+
+
+
