ee0f56b6484b3db6eb28717131657920ce8e16cc
007awais <116508346+007awais@users.noreply.github.com>
2024-11-25 14:08:08 +0500

    Main First Commit

---
+++ FX_TRY/src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load the FXML file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        
+        // Set the scene with the loaded FXML
+        Scene scene = new Scene(loader.load());
+        
+        // Set up the stage (window)
+        primaryStage.setTitle("Login Page");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
